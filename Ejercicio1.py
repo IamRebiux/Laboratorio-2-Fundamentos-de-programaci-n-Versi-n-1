cd326b9e515c4f37ff0e7893757fc8062b51fe3d
JeremyFuentes <126433727+JeremyFuentes@users.noreply.github.com>
2023-03-30 14:52:54 -0600

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,16 +1,28 @@
 costo= float(input("Ingrese el costo de su producto: \n ----->"))
-
 pago= float(input("Ingresse el efectivo con el que pagara: \n ----->"))
 
-if costo < pago:
-    vuelto = pago - costo 
-    print(f"Su cambio es de: $ {vuelto}")
+while pago < costo:
 
-elif costo == pago:
-    print(f"La cantidad es exacta (No tiene cambio) \n ¡Gracias por su compra!")
+    if pago > costo:
+        vuelto = pago - costo 
+        print(f"Su cambio es de: $ {vuelto}")
 
-elif costo > pago: 
-    costoFaltante = costo - pago 
-    print(f"Usted debe ${costoFaltante} ¡por favor pagar completo!")
+    elif costo == pago:
+        print(f"La cantidad es exacta (No tiene cambio) \n ¡Gracias por su compra!")
 
+    elif costo > pago: 
+        costoFaltante = costo - pago
+        print(f"Usted debe ${costoFaltante} ¡por favor pagar completo!")
+        pagofaltante = int(input())
+        pago = pagofaltante + pago
+
+if pago > costo:
+        vuelto = pago - costo 
+        print(f"Su cambio es de: $ {vuelto}")
+
+if costo == pago:
+        print(f"La cantidad es exacta (No tiene cambio) \n ¡Gracias por su compra!")
+if costo == pago:
+        print(f"La cantidad es exacta (No tiene cambio) \n ¡Gracias por su compra!")
+        
 print("FIN")
