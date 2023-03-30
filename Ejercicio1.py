51efa23810516498b9dd05118fcd361df81ee8b7
JeremyFuentes <126433727+JeremyFuentes@users.noreply.github.com>
2023-03-30 15:33:59 -0600

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,26 +1,21 @@
-costo= float(input("Ingrese el costo de su producto: \n ----->"))
+#Suma de cantidades igresadas y vuelto de pago ingresado
+
+#Datos pedidos al usuario
+costo= float(input("\nIngrese el costo de su producto: \n ----->"))
 pago= float(input("Ingresse el efectivo con el que pagara: \n ----->"))
 
-while pago < costo:
-
-    if pago > costo:
-        vuelto = pago - costo 
-        print(f"Su cambio es de: $ {vuelto}")
-
-    elif costo == pago:
-        print(f"La cantidad es exacta (No tiene cambio) \n ¡Gracias por su compra!")
-
-    elif costo > pago: 
-        costoFaltante = costo - pago
-        print(f"Usted debe ${costoFaltante} ¡por favor pagar completo!")
-        pagofaltante = int(input())
-        pago = pagofaltante + pago
+#Evaluacion del efectivo pagado
+while pago < costo: 
+    costoFaltante = costo - pago
+    print(f"Usted debe ${costoFaltante.round} ¡por favor pagar completo!").
+    pagofaltante = float(input("Ingrese el pago Restante \n --> "))
+    pago += pagofaltante
 
 if pago > costo:
         vuelto = pago - costo 
-        print(f"Su cambio es de: $ {vuelto}")
+        print(f"Su cambio es de: $ {vuelto}\n")
 
 if costo == pago:
-        print(f"La cantidad es exacta (No tiene cambio) \n ¡Gracias por su compra!")
+        print(f"La cantidad es exacta (No tiene cambio) \n ¡Gracias por su compra!\n")
         
 print("FIN")
